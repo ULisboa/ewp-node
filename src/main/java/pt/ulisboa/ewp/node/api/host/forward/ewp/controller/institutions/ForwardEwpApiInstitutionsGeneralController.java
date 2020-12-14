955035d1839db2bf35d48f6c002d3153fadddc71
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2020-12-14 14:53:34 +0000

    chore: move get all HEI IDs Forward EWP API endpoint to general Institutions controller

--- src/main/java/pt/ulisboa/ewp/node/api/host/forward/ewp/controller/institutions/ForwardEwpApiInstitutionsGeneralController.java
+++ src/main/java/pt/ulisboa/ewp/node/api/host/forward/ewp/controller/institutions/ForwardEwpApiInstitutionsGeneralController.java
@@ -1,13 +1,21 @@
 package pt.ulisboa.ewp.node.api.host.forward.ewp.controller.institutions;
 
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Collection;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.ewp.node.api.ewp.utils.EwpApiConstants;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.controller.AbstractForwardEwpApiController;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.controller.ForwardEwpApi;
+import pt.ulisboa.ewp.node.api.host.forward.ewp.dto.ForwardEwpApiHeiIdsResponseDTO;
+import pt.ulisboa.ewp.node.api.host.forward.ewp.dto.ForwardEwpApiResponseWithData;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.security.ForwardEwpApiSecurityCommonConstants;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.utils.ForwardEwpApiConstants;
+import pt.ulisboa.ewp.node.api.host.forward.ewp.utils.ForwardEwpApiResponseUtils;
 import pt.ulisboa.ewp.node.client.ewp.registry.RegistryClient;
 
 @RestController
@@ -18,6 +26,18 @@ public class ForwardEwpApiInstitutionsGeneralController extends AbstractForwardE
 
   public ForwardEwpApiInstitutionsGeneralController(RegistryClient registryClient) {
     super(registryClient);
+  }
+
+  @GetMapping(value = "/hei-ids", produces = MediaType.APPLICATION_XML_VALUE)
+  @Operation(
+      summary = "EWP Institutions Forward API.",
+      tags = {"Institutions"})
+  public ResponseEntity<ForwardEwpApiResponseWithData<ForwardEwpApiHeiIdsResponseDTO>>
+      getAllHeiIds() {
+    Collection<String> heiIds = getRegistryClient().getAllHeiIds();
+    return ResponseEntity.ok(
+        ForwardEwpApiResponseUtils.createResponseWithMessagesAndData(
+            new ForwardEwpApiHeiIdsResponseDTO(heiIds)));
   }
 
   @Override
--- src/main/java/pt/ulisboa/ewp/node/api/host/forward/ewp/controller/institutions/ForwardEwpApiInstitutionsV2Controller.java
+++ src/main/java/pt/ulisboa/ewp/node/api/host/forward/ewp/controller/institutions/ForwardEwpApiInstitutionsV2Controller.java
@@ -2,7 +2,6 @@ package pt.ulisboa.ewp.node.api.host.forward.ewp.controller.institutions;
 
 import eu.erasmuswithoutpaper.api.institutions.v2.InstitutionsResponseV2;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.Collection;
 import javax.validation.Valid;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.MediaType;
@@ -15,12 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.controller.AbstractForwardEwpApiController;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.controller.ForwardEwpApi;
-import pt.ulisboa.ewp.node.api.host.forward.ewp.dto.ForwardEwpApiHeiIdsResponseDTO;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.dto.ForwardEwpApiResponseWithData;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.dto.institutions.InstitutionsRequestDto;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.security.ForwardEwpApiSecurityCommonConstants;
 import pt.ulisboa.ewp.node.api.host.forward.ewp.utils.ForwardEwpApiConstants;
-import pt.ulisboa.ewp.node.api.host.forward.ewp.utils.ForwardEwpApiResponseUtils;
 import pt.ulisboa.ewp.node.client.ewp.EwpInstitutionsClient;
 import pt.ulisboa.ewp.node.client.ewp.exception.AbstractEwpClientErrorException;
 import pt.ulisboa.ewp.node.client.ewp.operation.result.success.EwpSuccessOperationResult;
@@ -38,18 +35,6 @@ public class ForwardEwpApiInstitutionsV2Controller extends AbstractForwardEwpApi
       RegistryClient registryClient, EwpInstitutionsClient client) {
     super(registryClient);
     this.client = client;
-  }
-
-  @GetMapping(value = "/hei-ids", produces = MediaType.APPLICATION_XML_VALUE)
-  @Operation(
-      summary = "EWP Institutions Forward API.",
-      tags = {"Institutions"})
-  public ResponseEntity<ForwardEwpApiResponseWithData<ForwardEwpApiHeiIdsResponseDTO>>
-      getAllHeiIds() {
-    Collection<String> heiIds = getRegistryClient().getAllHeiIds();
-    return ResponseEntity.ok(
-        ForwardEwpApiResponseUtils.createResponseWithMessagesAndData(
-            new ForwardEwpApiHeiIdsResponseDTO(heiIds)));
   }
 
   @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
