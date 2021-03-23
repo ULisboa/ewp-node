8061f25e2975d2c54ccaaebf1a14b9aae3a3289e
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2021-03-23 14:22:28 +0000

    refactor: extract some methods from EwpApiTlsCertificateAuthenticationFilter

--- src/main/java/pt/ulisboa/ewp/node/api/ewp/security/filter/EwpApiTlsCertificateAuthenticationFilter.java
+++ src/main/java/pt/ulisboa/ewp/node/api/ewp/security/filter/EwpApiTlsCertificateAuthenticationFilter.java
@@ -1,19 +1,15 @@
 package pt.ulisboa.ewp.node.api.ewp.security.filter;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -29,14 +25,13 @@ import pt.ulisboa.ewp.node.domain.entity.api.ewp.auth.EwpAuthenticationMethod;
 import pt.ulisboa.ewp.node.utils.CertificateUtils;
 import pt.ulisboa.ewp.node.utils.LoggerUtils;
 
-/** Filter that attempts to authenticate a request by TLS Certificate. */
+/**
+ * Filter that attempts to authenticate a request by TLS Certificate.
+ */
 public class EwpApiTlsCertificateAuthenticationFilter extends OncePerRequestFilter {
 
-  private static final String BEGIN_CERTIFICATE = "-----BEGIN CERTIFICATE-----";
-  private static final String END_CERTIFICATE = "-----END CERTIFICATE-----";
-
-  private SecurityProperties securityProperties;
-  private RegistryClient registryClient;
+  private final SecurityProperties securityProperties;
+  private final RegistryClient registryClient;
 
   public EwpApiTlsCertificateAuthenticationFilter(
       SecurityProperties securityProperties, RegistryClient registryClient) {
@@ -82,35 +77,65 @@ public class EwpApiTlsCertificateAuthenticationFilter extends OncePerRequestFilt
   }
 
   private EwpApiAuthenticateMethodResponse authenticate(EwpApiHttpRequestWrapper request) {
-    X509Certificate[] certificates = null;
+    X509Certificate[] certificates;
+    try {
+      certificates = parseCertificatesFromRequest(request);
+    } catch (Exception e) {
+      logger.error("Failed to parse client certificate from request: " + e.getMessage());
+      return EwpApiAuthenticateMethodResponse.failureBuilder(
+          EwpAuthenticationMethod.TLS, "Failed to parse client certificate: " + e.getMessage())
+          .withResponseCode(HttpStatus.BAD_REQUEST)
+          .build();
+    }
 
+    if (certificates == null && !securityProperties.isAllowMissingClientCertificate()) {
+      return EwpApiAuthenticateMethodResponse.failureBuilder(
+          EwpAuthenticationMethod.TLS, "Request is not using authentication method")
+          .notUsingMethod()
+          .withRequiredMethodInfoFulfilled(false)
+          .withResponseCode(HttpStatus.BAD_REQUEST)
+          .build();
+    }
+
+    CertificateUtils.logCertificates(certificates);
+
+    X509Certificate certificate = registryClient.getCertificateKnownInEwpNetwork(certificates);
+    if (certificate == null && !securityProperties.isAllowMissingClientCertificate()) {
+      return EwpApiAuthenticateMethodResponse.failureBuilder(
+              EwpAuthenticationMethod.TLS,
+          "None of the client certificates is valid in the EWP network")
+          .withResponseCode(HttpStatus.FORBIDDEN)
+          .build();
+    }
+
+    return EwpApiAuthenticateMethodResponse.successBuilder(
+        EwpAuthenticationMethod.TLS, registryClient.getHeisCoveredByCertificate(certificate))
+        .build();
+  }
+
+  private X509Certificate[] parseCertificatesFromRequest(EwpApiHttpRequestWrapper request)
+      throws IOException, CertificateException, DecoderException {
+    X509Certificate[] certificates = null;
     if (securityProperties.getClientTls().getHeaderName() != null) {
       String clientTlsHeaderName = securityProperties.getClientTls().getHeaderName();
-      try {
-        String encodedCertificateString = request.getHeader(clientTlsHeaderName);
-        if (StringUtils.isNotEmpty(encodedCertificateString)) {
-          logger.debug(
-              "Received header "
-                  + clientTlsHeaderName
-                  + " with value: \n"
-                  + encodedCertificateString);
-          String certificateString =
-              URLDecoder.decode(encodedCertificateString, StandardCharsets.UTF_8.name())
-                  .replaceAll("[\r\n\t]", "");
-          X509Certificate certificate = decodeCertificate(certificateString);
-          if (certificate != null) {
-            certificates = new X509Certificate[] {certificate};
-          } else {
-            logger.error("Failed to decode certificate from header: " + encodedCertificateString);
-          }
+      String encodedCertificateString = request.getHeader(clientTlsHeaderName);
+      if (StringUtils.isNotEmpty(encodedCertificateString)) {
+        logger.debug(
+            "Received header "
+                + clientTlsHeaderName
+                + " with value: \n"
+                + encodedCertificateString);
+        String certificateString =
+            URLDecoder.decode(encodedCertificateString, StandardCharsets.UTF_8.name())
+                .replaceAll("[\r\n\t]", "");
+        X509Certificate certificate =
+            CertificateUtils.decodeCertificate(
+                certificateString, securityProperties.getClientTls().getEncoding());
+        if (certificate != null) {
+          certificates = new X509Certificate[]{certificate};
+        } else {
+          logger.error("Failed to decode certificate from header: " + encodedCertificateString);
         }
-      } catch (Exception e) {
-        logger.error("Failed to parse client certificate from request: " + e.getMessage());
-        return EwpApiAuthenticateMethodResponse.failureBuilder(
-                EwpAuthenticationMethod.TLS,
-                "Failed to parse client certificate: " + e.getMessage())
-            .withResponseCode(HttpStatus.BAD_REQUEST)
-            .build();
       }
     }
 
@@ -119,66 +144,6 @@ public class EwpApiTlsCertificateAuthenticationFilter extends OncePerRequestFilt
           (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
     }
 
-    if (certificates == null && !securityProperties.isAllowMissingClientCertificate()) {
-      return EwpApiAuthenticateMethodResponse.failureBuilder(
-              EwpAuthenticationMethod.TLS, "Request is not using authentication method")
-          .notUsingMethod()
-          .withRequiredMethodInfoFulfilled(false)
-          .withResponseCode(HttpStatus.BAD_REQUEST)
-          .build();
-    }
-
-    if (certificates != null) {
-      logger.info("Received certificates: ");
-      for (X509Certificate certificate : certificates) {
-        String fingerprint = CertificateUtils.extractFingerprint(certificate);
-        if (certificate.getSubjectDN() != null) {
-          logger.info(
-              "\t- "
-                  + fingerprint
-                  + ": (Subject DN: "
-                  + certificate.getSubjectDN().getName()
-                  + ")");
-        } else {
-          logger.info("\t- " + fingerprint + ": (Without subject DN)");
-        }
-      }
-    }
-
-    X509Certificate certificate = registryClient.getCertificateKnownInEwpNetwork(certificates);
-    if (certificate == null && !securityProperties.isAllowMissingClientCertificate()) {
-      return EwpApiAuthenticateMethodResponse.failureBuilder(
-              EwpAuthenticationMethod.TLS,
-              "None of the client certificates is valid in the EWP network")
-          .withResponseCode(HttpStatus.FORBIDDEN)
-          .build();
-    }
-
-    return EwpApiAuthenticateMethodResponse.successBuilder(
-            EwpAuthenticationMethod.TLS, registryClient.getHeisCoveredByCertificate(certificate))
-        .build();
-  }
-
-  private X509Certificate decodeCertificate(String certificateString)
-      throws CertificateException, DecoderException {
-    String sanitizedCertificateString =
-        certificateString.replace(BEGIN_CERTIFICATE, "").replace(END_CERTIFICATE, "");
-    byte[] decodedCertificate;
-    switch (securityProperties.getClientTls().getEncoding()) {
-      case BASE64:
-        decodedCertificate = Base64.decodeBase64(sanitizedCertificateString);
-        break;
-
-      case HEX:
-        decodedCertificate = Hex.decodeHex(sanitizedCertificateString);
-        break;
-
-      default:
-        throw new IllegalStateException(
-            "Unknown encoding: " + securityProperties.getClientTls().getEncoding());
-    }
-    return (X509Certificate)
-        CertificateFactory.getInstance("X.509")
-            .generateCertificate(new ByteArrayInputStream(decodedCertificate));
+    return certificates;
   }
 }
--- src/main/java/pt/ulisboa/ewp/node/utils/CertificateUtils.java
+++ src/main/java/pt/ulisboa/ewp/node/utils/CertificateUtils.java
@@ -10,10 +10,22 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Locale;
 import javax.xml.bind.DatatypeConverter;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.ewp.node.config.security.SecurityClientTlsEncoding;
 
 public class CertificateUtils {
 
-  private CertificateUtils() {}
+  private static final Logger LOGGER = LoggerFactory.getLogger(CertificateUtils.class);
+
+  private static final String BEGIN_CERTIFICATE = "-----BEGIN CERTIFICATE-----";
+  private static final String END_CERTIFICATE = "-----END CERTIFICATE-----";
+
+  private CertificateUtils() {
+  }
 
   public static X509Certificate parseCertificate(byte[] certificateBytes)
       throws CertificateException {
@@ -22,12 +34,35 @@ public class CertificateUtils {
     return (X509Certificate) certificateFactory.generateCertificate(certificateInputStream);
   }
 
+  public static X509Certificate decodeCertificate(
+      String certificateString, SecurityClientTlsEncoding encoding)
+      throws CertificateException, DecoderException {
+    String sanitizedCertificateString =
+        certificateString.replace(BEGIN_CERTIFICATE, "").replace(END_CERTIFICATE, "");
+    byte[] decodedCertificate;
+    switch (encoding) {
+      case BASE64:
+        decodedCertificate = Base64.decodeBase64(sanitizedCertificateString);
+        break;
+
+      case HEX:
+        decodedCertificate = Hex.decodeHex(sanitizedCertificateString);
+        break;
+
+      default:
+        throw new IllegalStateException("Unknown encoding: " + encoding);
+    }
+    return (X509Certificate)
+        CertificateFactory.getInstance("X.509")
+            .generateCertificate(new ByteArrayInputStream(decodedCertificate));
+  }
+
   public static String extractFingerprint(Certificate cert) {
     MessageDigest md;
     try {
       md = MessageDigest.getInstance("SHA-256");
     } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException(e);
+      throw new IllegalStateException(e);
     }
     try {
       byte[] encoded = cert.getEncoded();
@@ -36,9 +71,27 @@ public class CertificateUtils {
       }
       md.update(encoded);
     } catch (CertificateEncodingException e) {
-      throw new RuntimeException(e);
+      throw new IllegalStateException(e);
     }
     byte[] binDigest = md.digest();
     return DatatypeConverter.printHexBinary(binDigest).toLowerCase(Locale.ENGLISH);
+  }
+
+  public static void logCertificates(X509Certificate[] certificates) {
+    if (certificates != null) {
+      LOGGER.info("Certificates: ");
+      for (X509Certificate certificate : certificates) {
+        logCertificate(certificate);
+      }
+    }
+  }
+
+  public static void logCertificate(X509Certificate certificate) {
+    String fingerprint = CertificateUtils.extractFingerprint(certificate);
+    if (certificate.getSubjectDN() != null) {
+      LOGGER.info("\t- {}: (Subject DN: {})", fingerprint, certificate.getSubjectDN().getName());
+    } else {
+      LOGGER.info("\t- {}: (Without subject DN)", fingerprint);
+    }
   }
 }
