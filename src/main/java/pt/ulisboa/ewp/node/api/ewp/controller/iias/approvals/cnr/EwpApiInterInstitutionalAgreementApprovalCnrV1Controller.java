34da2e83ca691ab0db4fb451e12f32a53aa1d61f
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2021-11-22 16:01:56 +0000

    feat: add support to EWP InterInstitutional Agreement Approval CNR V1 API as producer

---
+++ src/main/java/pt/ulisboa/ewp/node/api/ewp/controller/iias/approvals/cnr/EwpApiInterInstitutionalAgreementApprovalCnrV1Controller.java
@@ -0,0 +1,53 @@
+package pt.ulisboa.ewp.node.api.ewp.controller.iias.approvals.cnr;
+
+import eu.erasmuswithoutpaper.api.architecture.v1.EmptyV1;
+import eu.erasmuswithoutpaper.api.iias.approval.cnr.v1.IiaApprovalCnrResponseV1;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Collection;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.ewp.host.plugin.skeleton.provider.iias.approval.cnr.InterInstitutionalAgreementApprovalCnrV1HostProvider;
+import pt.ulisboa.ewp.node.api.ewp.controller.EwpApi;
+import pt.ulisboa.ewp.node.api.ewp.utils.EwpApiConstants;
+import pt.ulisboa.ewp.node.api.ewp.utils.EwpApiParamConstants;
+import pt.ulisboa.ewp.node.plugin.manager.host.HostPluginManager;
+
+@RestController
+@EwpApi
+@RequestMapping(
+    EwpApiConstants.API_BASE_URI
+        + EwpApiInterInstitutionalAgreementApprovalCnrV1Controller.BASE_PATH)
+public class EwpApiInterInstitutionalAgreementApprovalCnrV1Controller {
+
+  public static final String BASE_PATH = "iias/approvals/cnr/v1";
+
+  private final HostPluginManager hostPluginManager;
+
+  public EwpApiInterInstitutionalAgreementApprovalCnrV1Controller(
+      HostPluginManager hostPluginManager) {
+    this.hostPluginManager = hostPluginManager;
+  }
+
+  @RequestMapping(method = {RequestMethod.GET,
+      RequestMethod.POST}, produces = MediaType.APPLICATION_XML_VALUE)
+  @Operation(
+      summary = "IIA Approval CNR API.",
+      tags = {"ewp"})
+  public ResponseEntity<IiaApprovalCnrResponseV1> iiaCnr(
+      @RequestParam(value = EwpApiParamConstants.APPROVING_HEI_ID) String approvingHeiId,
+      @RequestParam(value = EwpApiParamConstants.OWNER_HEI_ID) String ownerHeiId,
+      @RequestParam(value = EwpApiParamConstants.IIA_ID) String iiaId) {
+
+    Collection<InterInstitutionalAgreementApprovalCnrV1HostProvider> providers = hostPluginManager.getAllProvidersOfType(
+        InterInstitutionalAgreementApprovalCnrV1HostProvider.class);
+    for (InterInstitutionalAgreementApprovalCnrV1HostProvider provider : providers) {
+      provider.onChangeNotification(approvingHeiId, ownerHeiId, iiaId);
+    }
+
+    return ResponseEntity.ok(new IiaApprovalCnrResponseV1(new EmptyV1()));
+  }
+}
---
+++ src/test/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/iias/approvals/cnr/MockInterInstitutionalAgreementApprovalCnrV1HostProvider.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.ewp.host.plugin.skeleton.provider.iias.approvals.cnr;
+
+import pt.ulisboa.ewp.host.plugin.skeleton.provider.iias.approval.cnr.InterInstitutionalAgreementApprovalCnrV1HostProvider;
+
+public class MockInterInstitutionalAgreementApprovalCnrV1HostProvider extends
+    InterInstitutionalAgreementApprovalCnrV1HostProvider {
+
+  @Override
+  public void onChangeNotification(String approvingHeiId, String ownerHeiId, String iiaId) {
+    
+  }
+}
