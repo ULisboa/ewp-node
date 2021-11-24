830c9018c8d813ca00b4405e03bb623c2446f4f8
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2021-11-24 15:13:02 +0000

    tests: improve EwpApiOutgoingMobilitiesManifestEntryProviderTest

--- src/test/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesManifestEntryProviderTest.java
+++ src/test/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesManifestEntryProviderTest.java
@@ -5,14 +5,13 @@ import static org.mockito.Mockito.doReturn;
 
 import eu.erasmuswithoutpaper.api.architecture.v1.ManifestApiEntryBaseV1;
 import eu.erasmuswithoutpaper.api.omobilities.v1.OmobilitiesV1;
-import eu.erasmuswithoutpaper.api.omobilities.v1.endpoints.StudentMobilityForStudiesV1;
-import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import javax.annotation.Nullable;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pt.ulisboa.ewp.host.plugin.skeleton.provider.HostProvider;
+import pt.ulisboa.ewp.host.plugin.skeleton.provider.omobilities.MockOutgoingMobilitiesV1HostProvider;
 import pt.ulisboa.ewp.host.plugin.skeleton.provider.omobilities.OutgoingMobilitiesV1HostProvider;
 import pt.ulisboa.ewp.node.plugin.manager.host.HostPluginManager;
 
@@ -47,22 +46,7 @@ class EwpApiOutgoingMobilitiesManifestEntryProviderTest {
     String heiId = "abc";
     String baseUrl = "http://example.com";
 
-    OutgoingMobilitiesV1HostProvider provider = new OutgoingMobilitiesV1HostProvider() {
-
-      @Override
-      public Collection<String> findOutgoingMobilityIds(Collection<String> requesterCoveredHeiIds,
-          String sendingHeiId, Collection<String> receivingHeiIds,
-          @Nullable String receivingAcademicYearId, @Nullable LocalDateTime modifiedSince) {
-        return null;
-      }
-
-      @Override
-      public Collection<StudentMobilityForStudiesV1> findBySendingHeiIdAndOutgoingMobilityIds(
-          Collection<String> requesterCoveredHeiIds, String sendingHeiId,
-          Collection<String> outgoingMobilityIds) {
-        return null;
-      }
-    };
+    OutgoingMobilitiesV1HostProvider provider = new MockOutgoingMobilitiesV1HostProvider(5);
 
     doReturn(Collections.singletonList(provider)).when(hostPluginManager)
         .getAllProvidersOfType(heiId, HostProvider.class);
@@ -82,6 +66,40 @@ class EwpApiOutgoingMobilitiesManifestEntryProviderTest {
         baseUrl + EwpApiOutgoingMobilitiesV1Controller.BASE_PATH + "/get");
     assertThat(manifestEntry.getMaxOmobilityIds()).isEqualTo(
         provider.getMaxOutgoingMobilityIdsPerRequest());
+    assertThat(manifestEntry.getSendsNotifications()).isNotNull();
+  }
+
+  @Test
+  void testGetManifestEntries_TwoRegisteredV1Provider_ManifestEntryReturned() {
+    // Arrange
+    HostPluginManager hostPluginManager = Mockito.spy(new HostPluginManager(""));
+    EwpApiOutgoingMobilitiesManifestEntryProvider manifestEntryProvider = new EwpApiOutgoingMobilitiesManifestEntryProvider(
+        hostPluginManager);
+    String heiId = "abc";
+    String baseUrl = "http://example.com";
+
+    OutgoingMobilitiesV1HostProvider provider1 = new MockOutgoingMobilitiesV1HostProvider(5);
+
+    OutgoingMobilitiesV1HostProvider provider2 = new MockOutgoingMobilitiesV1HostProvider(10);
+
+    doReturn(Arrays.asList(provider1, provider2)).when(hostPluginManager)
+        .getAllProvidersOfType(heiId, HostProvider.class);
+
+    // Act
+    Collection<ManifestApiEntryBaseV1> manifestEntries = manifestEntryProvider.getManifestEntries(
+        heiId, baseUrl);
+
+    // Assert
+    assertThat(manifestEntries).hasSize(1);
+    assertThat(manifestEntries.iterator().next()).isInstanceOf(OmobilitiesV1.class);
+    OmobilitiesV1 manifestEntry = (OmobilitiesV1) manifestEntries.iterator().next();
+    assertThat(manifestEntry.getVersion()).isEqualTo(provider1.getVersion());
+    assertThat(manifestEntry.getIndexUrl()).isEqualTo(
+        baseUrl + EwpApiOutgoingMobilitiesV1Controller.BASE_PATH + "/index");
+    assertThat(manifestEntry.getGetUrl()).isEqualTo(
+        baseUrl + EwpApiOutgoingMobilitiesV1Controller.BASE_PATH + "/get");
+    assertThat(manifestEntry.getMaxOmobilityIds()).isEqualTo(
+        provider1.getMaxOutgoingMobilityIdsPerRequest());
     assertThat(manifestEntry.getSendsNotifications()).isNotNull();
   }
 
