7e0022c8658f762967568c3b6e1daf073c300c18
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2022-12-28 15:29:36 +0000

    improvement: only do not log requests to EWP Manifest API if the request was successful
    Previously, all requests to EWP Manifest API were not logged. Now, error responses are logged.

--- src/main/java/pt/ulisboa/ewp/node/service/http/log/ewp/EwpHttpCommunicationLogService.java
+++ src/main/java/pt/ulisboa/ewp/node/service/http/log/ewp/EwpHttpCommunicationLogService.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Collections;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 import pt.ulisboa.ewp.node.api.ewp.wrapper.EwpApiHttpRequestWrapper;
@@ -39,7 +40,7 @@ public class EwpHttpCommunicationLogService extends HttpCommunicationLogService 
       String observations) {
 
     // NOTE: Requests for manifest are not logged to avoid using too much log space
-    if (isRequestForManifest(request)) {
+    if (isRequestForManifest(request) && response.getStatus() == HttpStatus.OK.value()) {
       return;
     }
 
