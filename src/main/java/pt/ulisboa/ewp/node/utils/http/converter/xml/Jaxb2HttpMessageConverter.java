5cbad2bef7e44b46dc83409491d52b20c6ee1e84
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2023-01-09 16:34:27 +0000

    feat: remove unused namespaces on success responses using XSLT

--- src/main/java/pt/ulisboa/ewp/node/utils/http/converter/xml/Jaxb2HttpMessageConverter.java
+++ src/main/java/pt/ulisboa/ewp/node/utils/http/converter/xml/Jaxb2HttpMessageConverter.java
@@ -1,12 +1,19 @@
 package pt.ulisboa.ewp.node.utils.http.converter.xml;
 
 import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -49,13 +56,31 @@ public class Jaxb2HttpMessageConverter extends Jaxb2RootElementHttpMessageConver
       // If successful, it ensures that the main namespace uses an empty namespace prefix.
       Class<?> clazz = ClassUtils.getUserClass(object);
       Jaxb2Marshaller marshaller = createJaxb2Marshaller(clazz);
-      marshaller.marshal(object, result);
+      marshal(marshaller, object, result);
 
     } catch (XmlMappingException e) {
       // NOTE: If the marshalling failed, then marshall using all known packages as context.
       Jaxb2Marshaller marshaller = createJaxb2Marshaller(null);
-      marshaller.marshal(object, result);
+      marshal(marshaller, object, result);
     }
+  }
+
+  private void marshal(Jaxb2Marshaller marshaller, Object object, Result result)
+      throws TransformerException {
+    TransformerFactory tf = TransformerFactory.newInstance();
+    StreamSource xslt = new StreamSource(
+        getClass().getClassLoader().getResourceAsStream("xsl/remove-unused-namespaces.xsl"));
+    Transformer transformer = tf.newTransformer(xslt);
+
+    // NOTE: Marshall object to XML
+    ByteArrayOutputStream intermediaryOutputStream = new ByteArrayOutputStream();
+    StreamResult intermediaryResult = new StreamResult(intermediaryOutputStream);
+    marshaller.marshal(object, intermediaryResult);
+
+    // NOTE: Use XSLT to remove unused namespaces
+    ByteArrayInputStream finalInputStream = new ByteArrayInputStream(
+        intermediaryOutputStream.toByteArray());
+    transformer.transform(new StreamSource(finalInputStream), result);
   }
 
   protected final Jaxb2Marshaller createJaxb2Marshaller(Class<?> clazz) {
