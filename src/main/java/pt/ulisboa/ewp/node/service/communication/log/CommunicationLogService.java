367c69ae898e342ef56e212cf82a6c8a833d3a4b
Fábio Ferreira <fabioferreira@campus.ul.pt>
2023-08-01 12:30:19 +0100

    refactor: add registerExceptionInCurrentCommunication(..) to CommunicationLogService

--- src/main/java/pt/ulisboa/ewp/node/service/communication/log/CommunicationLogService.java
+++ src/main/java/pt/ulisboa/ewp/node/service/communication/log/CommunicationLogService.java
@@ -3,18 +3,31 @@ package pt.ulisboa.ewp.node.service.communication.log;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import javax.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pt.ulisboa.ewp.node.domain.entity.communication.log.CommunicationLog;
 import pt.ulisboa.ewp.node.domain.repository.communication.log.CommunicationLogRepository;
+import pt.ulisboa.ewp.node.service.communication.context.CommunicationContextHolder;
 
 @Service
 @Transactional
 public class CommunicationLogService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(CommunicationLogService.class);
+
   private final CommunicationLogRepository repository;
 
   public CommunicationLogService(CommunicationLogRepository repository) {
     this.repository = repository;
+  }
+
+  public void registerExceptionInCurrentCommunication(Exception exception) {
+    if (CommunicationContextHolder.getContext().getCurrentCommunicationLog() != null) {
+      this.registerException(CommunicationContextHolder.getContext().getCurrentCommunicationLog(), exception);
+    } else {
+      throw new IllegalStateException("No current communication found");
+    }
   }
 
   public void registerException(CommunicationLog communicationLog, Exception exception) {
