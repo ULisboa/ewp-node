f4af11f0682698f0725c5eb0e4bb328bf02322a7
Fábio Ferreira <fabioferreira@campus.ul.pt>
2023-08-07 13:15:30 +0100

    fix: cacerts path of registry service was incorrect

--- docker/registry/dev/data/entrypoint.sh
+++ docker/registry/dev/data/entrypoint.sh
@@ -5,7 +5,7 @@
 git init repo
 
 # Import localhost certificate to CACERTS
-CACERTS_STORE="/usr/local/openjdk-8/lib/security/cacerts"
+CACERTS_STORE="/opt/java/openjdk/lib/security/cacerts"
 KEYSTORE_PATH=/opt/keystore.p12
 keytool -v -importkeystore -srckeystore ${KEYSTORE_PATH} -srcstoretype PKCS12 -srcstorepass p@ssw0rd -destkeystore $CACERTS_STORE -deststoretype JKS -deststorepass changeit -noprompt
 
