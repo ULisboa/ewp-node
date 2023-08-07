a0362bd03d65422b8a099dd6e9d06f28f538c0f7
Fábio Ferreira <fabioferreira@campus.ul.pt>
2023-08-07 09:42:46 +0100

    refactor: minor refactor of CommunicationLogService

--- src/main/java/pt/ulisboa/ewp/node/service/communication/log/CommunicationLogService.java
+++ src/main/java/pt/ulisboa/ewp/node/service/communication/log/CommunicationLogService.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pt.ulisboa.ewp.node.domain.entity.communication.log.CommunicationLog;
+import pt.ulisboa.ewp.node.domain.repository.AbstractRepository;
 import pt.ulisboa.ewp.node.domain.repository.communication.log.CommunicationLogRepository;
 import pt.ulisboa.ewp.node.service.communication.context.CommunicationContextHolder;
 
@@ -22,20 +23,26 @@ public class CommunicationLogService {
     this.repository = repository;
   }
 
-  public void registerExceptionInCurrentCommunication(Exception exception) {
+  public static <T extends CommunicationLog> void registerException(
+      AbstractRepository<T> repository, T communicationLog, Throwable throwable) {
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter printWriter = new PrintWriter(stringWriter);
+    throwable.printStackTrace(printWriter);
+
+    communicationLog.setExceptionStacktrace(stringWriter.toString());
+    repository.persist(communicationLog);
+  }
+
+  public void registerExceptionInCurrentCommunication(Throwable throwable) {
     if (CommunicationContextHolder.getContext().getCurrentCommunicationLog() != null) {
-      this.registerException(CommunicationContextHolder.getContext().getCurrentCommunicationLog(), exception);
+      this.registerException(
+          CommunicationContextHolder.getContext().getCurrentCommunicationLog(), throwable);
     } else {
       LOG.warn("No current communication found, ignoring exception...");
     }
   }
 
-  public void registerException(CommunicationLog communicationLog, Exception exception) {
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter printWriter = new PrintWriter(stringWriter);
-    exception.printStackTrace(printWriter);
-
-    communicationLog.setExceptionStacktrace(stringWriter.toString());
-    repository.persist(communicationLog);
+  public void registerException(CommunicationLog communicationLog, Throwable throwable) {
+    registerException(repository, communicationLog, throwable);
   }
 }
