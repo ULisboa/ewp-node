0d6348fcdd5bf541162966e1f64a62fc683fe6e1
Fábio Ferreira <fabioferreira@campus.ul.pt>
2024-02-26 14:44:50 +0000

    refactor: make status of EwpChangeNotification be stored as string instead of ordinal

--- backend/src/main/java/pt/ulisboa/ewp/node/domain/entity/notification/EwpChangeNotification.java
+++ backend/src/main/java/pt/ulisboa/ewp/node/domain/entity/notification/EwpChangeNotification.java
@@ -7,6 +7,8 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -104,6 +106,7 @@ public abstract class EwpChangeNotification {
   }
 
   @Column(name = "status", nullable = false)
+  @Enumerated(EnumType.STRING)
   public Status getStatus() {
     return status;
   }
