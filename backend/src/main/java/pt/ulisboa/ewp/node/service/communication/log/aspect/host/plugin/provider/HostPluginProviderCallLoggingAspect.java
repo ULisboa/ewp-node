d8e206e94f3381f20f9bce75ee75ca0f8633b20e
Fábio Ferreira <fabioferreira@campus.ul.pt>
2024-02-28 14:34:34 +0000

    feat: avoid logging non relevant host plugin function calls
    Currently, methods that match the pattern "getMax(.*)PerRequest" are not logged

--- backend/src/main/java/pt/ulisboa/ewp/node/service/communication/log/aspect/host/plugin/provider/HostPluginProviderCallLoggingAspect.java
+++ backend/src/main/java/pt/ulisboa/ewp/node/service/communication/log/aspect/host/plugin/provider/HostPluginProviderCallLoggingAspect.java
@@ -3,6 +3,7 @@ package pt.ulisboa.ewp.node.service.communication.log.aspect.host.plugin.provide
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -13,7 +14,6 @@ import pt.ulisboa.ewp.host.plugin.skeleton.provider.HostProvider;
 import pt.ulisboa.ewp.node.domain.entity.communication.log.CommunicationLog;
 import pt.ulisboa.ewp.node.domain.entity.communication.log.host.plugin.HostPluginFunctionCallCommunicationLog;
 import pt.ulisboa.ewp.node.exception.domain.DomainException;
-import pt.ulisboa.ewp.node.service.communication.context.CommunicationContext;
 import pt.ulisboa.ewp.node.service.communication.context.CommunicationContextHolder;
 import pt.ulisboa.ewp.node.service.communication.log.host.plugin.HostPluginFunctionCallCommunicationLogService;
 import pt.ulisboa.ewp.node.utils.serialization.SerializationUtils;
@@ -22,6 +22,9 @@ import pt.ulisboa.ewp.node.utils.serialization.TypeAndString;
 @Aspect
 @Configuration
 public class HostPluginProviderCallLoggingAspect extends HostPluginProviderAspect {
+
+  private static final List<Pattern> LOGGING_BLACKLIST_METHOD_PATTERNS =
+      List.of(Pattern.compile("getMax(.*)PerRequest"));
 
   private final HostPluginFunctionCallCommunicationLogService communicationLogService;
 
@@ -40,52 +43,55 @@ public class HostPluginProviderCallLoggingAspect extends HostPluginProviderAspec
   public Object onFunctionCall(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
     return CommunicationContextHolder.runInNestedContext(
         (context) -> {
-          HostPluginFunctionCallCommunicationLog communicationLog;
+          HostPluginFunctionCallCommunicationLog communicationLog = null;
           HostProvider hostProvider = (HostProvider) proceedingJoinPoint.getTarget();
+          boolean logged = false;
           try {
-            communicationLog = createCommunicationLog(proceedingJoinPoint, context, hostProvider);
+            CommunicationLog parentCommunicationLog =
+                context.getParentContext() != null
+                    ? context.getParentContext().getCurrentCommunicationLog()
+                    : null;
+            HostPlugin hostPlugin = hostProvider.getPlugin();
+            String hostPluginId = hostPlugin.getWrapper().getPluginId();
+            String className = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+            String method = proceedingJoinPoint.getSignature().getName();
+            List<Object> arguments = Arrays.asList(proceedingJoinPoint.getArgs());
+            if (isNotBlacklistedToBeLogged(method)) {
+              communicationLog =
+                  this.communicationLogService.logCommunicationBeforeExecution(
+                      ZonedDateTime.now(),
+                      parentCommunicationLog,
+                      hostPluginId,
+                      className,
+                      method,
+                      arguments);
+              logged = true;
+            }
+
           } catch (DomainException e) {
             throw new IllegalStateException("Failed to prepare to process host plugin call", e);
           }
 
           try {
             Object result = proceedingJoinPoint.proceed();
-            TypeAndString resultTypeAndString = SerializationUtils.convertToTypeAndString(result);
-            this.communicationLogService.updateCommunicationAfterExecution(
-                communicationLog, resultTypeAndString.getType(), resultTypeAndString.getString());
+            if (logged) {
+              TypeAndString resultTypeAndString = SerializationUtils.convertToTypeAndString(result);
+              this.communicationLogService.updateCommunicationAfterExecution(
+                  communicationLog, resultTypeAndString.getType(), resultTypeAndString.getString());
+            }
             return result;
 
           } catch (Throwable throwable) {
-            communicationLogService.registerException(communicationLog, throwable);
+            if (logged) {
+              communicationLogService.registerException(communicationLog, throwable);
+            }
             throw new RuntimeException(throwable);
           }
         });
   }
 
-  private HostPluginFunctionCallCommunicationLog createCommunicationLog(
-      ProceedingJoinPoint proceedingJoinPoint,
-      CommunicationContext context,
-      HostProvider hostProvider)
-      throws DomainException {
-    HostPluginFunctionCallCommunicationLog communicationLog;
-    CommunicationLog parentCommunicationLog =
-        context.getParentContext() != null
-            ? context.getParentContext().getCurrentCommunicationLog()
-            : null;
-    HostPlugin hostPlugin = hostProvider.getPlugin();
-    String hostPluginId = hostPlugin.getWrapper().getPluginId();
-    String className = proceedingJoinPoint.getSignature().getDeclaringTypeName();
-    String method = proceedingJoinPoint.getSignature().getName();
-    List<Object> arguments = Arrays.asList(proceedingJoinPoint.getArgs());
-    communicationLog =
-        this.communicationLogService.logCommunicationBeforeExecution(
-            ZonedDateTime.now(),
-            parentCommunicationLog,
-            hostPluginId,
-            className,
-            method,
-            arguments);
-    return communicationLog;
+  private boolean isNotBlacklistedToBeLogged(String method) {
+    return LOGGING_BLACKLIST_METHOD_PATTERNS.stream().noneMatch(p -> p.matcher(method).matches());
   }
 
 }
