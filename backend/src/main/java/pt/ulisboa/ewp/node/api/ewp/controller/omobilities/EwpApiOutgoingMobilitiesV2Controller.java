12ed4e7c5b0cd301197c71e7e918ce3c205b9490
Fábio Ferreira <fabioferreira@edu.ulisboa.pt>
2024-07-16 15:23:14 +0100

    improvement: make EwpApiOutgoingMobilitiesV2Controller use a provider chain to get outgoing mobilities
    This is similar to the handling of IIA get requests.

--- backend/src/main/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesV2Controller.java
+++ backend/src/main/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesV2Controller.java
@@ -4,14 +4,13 @@ import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME
 
 import eu.erasmuswithoutpaper.api.omobilities.v2.endpoints.OmobilitiesGetResponseV2;
 import eu.erasmuswithoutpaper.api.omobilities.v2.endpoints.OmobilitiesIndexResponseV2;
+import eu.erasmuswithoutpaper.api.omobilities.v2.endpoints.StudentMobilityV2;
 import io.swagger.v3.oas.annotations.Operation;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -29,12 +28,13 @@ import pt.ulisboa.ewp.node.api.ewp.utils.EwpApiParamConstants;
 import pt.ulisboa.ewp.node.domain.entity.mapping.EwpOutgoingMobilityMapping;
 import pt.ulisboa.ewp.node.domain.repository.mapping.EwpOutgoingMobilityMappingRepository;
 import pt.ulisboa.ewp.node.exception.ewp.EwpBadRequestException;
+import pt.ulisboa.ewp.node.exception.ewp.EwpUnknownHeiIdException;
+import pt.ulisboa.ewp.node.exception.ewp.EwpUnknownOrganizationalUnitIdException;
 import pt.ulisboa.ewp.node.plugin.manager.host.HostPluginManager;
 
 @RestController
 @EwpApi
-@RequestMapping(
-    EwpApiConstants.API_BASE_URI + EwpApiOutgoingMobilitiesV2Controller.BASE_PATH)
+@RequestMapping(EwpApiConstants.API_BASE_URI + EwpApiOutgoingMobilitiesV2Controller.BASE_PATH)
 public class EwpApiOutgoingMobilitiesV2Controller {
 
   public static final String BASE_PATH = "omobilities/v2";
@@ -43,8 +43,8 @@ public class EwpApiOutgoingMobilitiesV2Controller {
 
   private final EwpOutgoingMobilityMappingRepository mappingRepository;
 
-  public EwpApiOutgoingMobilitiesV2Controller(HostPluginManager hostPluginManager,
-      EwpOutgoingMobilityMappingRepository mappingRepository) {
+  public EwpApiOutgoingMobilitiesV2Controller(
+      HostPluginManager hostPluginManager, EwpOutgoingMobilityMappingRepository mappingRepository) {
     this.hostPluginManager = hostPluginManager;
     this.mappingRepository = mappingRepository;
   }
@@ -72,17 +72,22 @@ public class EwpApiOutgoingMobilitiesV2Controller {
       throw new EwpBadRequestException("Unknown HEI ID: " + sendingHeiId);
     }
 
-    Collection<OutgoingMobilitiesV2HostProvider> providers = hostPluginManager.getAllProvidersOfType(
-        sendingHeiId, OutgoingMobilitiesV2HostProvider.class);
+    Collection<OutgoingMobilitiesV2HostProvider> providers =
+        hostPluginManager.getAllProvidersOfType(
+            sendingHeiId, OutgoingMobilitiesV2HostProvider.class);
 
     OmobilitiesIndexResponseV2 response = new OmobilitiesIndexResponseV2();
-    providers.forEach(provider -> {
-      Collection<String> outgoingMobilityIds = provider
-          .findOutgoingMobilityIds(authenticationToken.getPrincipal().getHeiIdsCoveredByClient(),
-              sendingHeiId, receivingHeiIds, receivingAcademicYearId,
-              modifiedSince);
-      response.getOmobilityId().addAll(outgoingMobilityIds);
-    });
+    providers.forEach(
+        provider -> {
+          Collection<String> outgoingMobilityIds =
+              provider.findOutgoingMobilityIds(
+                  authenticationToken.getPrincipal().getHeiIdsCoveredByClient(),
+                  sendingHeiId,
+                  receivingHeiIds,
+                  receivingAcademicYearId,
+                  modifiedSince);
+          response.getOmobilityId().addAll(outgoingMobilityIds);
+        });
     return ResponseEntity.ok(response);
   }
 
@@ -108,14 +113,16 @@ public class EwpApiOutgoingMobilitiesV2Controller {
         outgoingMobilityIds != null ? outgoingMobilityIds : Collections.emptyList();
 
     if (outgoingMobilityIds.isEmpty()) {
-      throw new EwpBadRequestException(
-          "At least some Outgoing Mobility ID must be provided");
+      throw new EwpBadRequestException("At least some Outgoing Mobility ID must be provided");
     }
 
-    int maxOmobilityIdsPerRequest = hostPluginManager.getAllProvidersOfType(sendingHeiId,
-            OutgoingMobilitiesV2HostProvider.class).stream().mapToInt(
-            OutgoingMobilitiesV2HostProvider::getMaxOutgoingMobilityIdsPerRequest)
-        .min().orElse(0);
+    int maxOmobilityIdsPerRequest =
+        hostPluginManager
+            .getAllProvidersOfType(sendingHeiId, OutgoingMobilitiesV2HostProvider.class)
+            .stream()
+            .mapToInt(OutgoingMobilitiesV2HostProvider::getMaxOutgoingMobilityIdsPerRequest)
+            .min()
+            .orElse(0);
 
     if (outgoingMobilityIds.size() > maxOmobilityIdsPerRequest) {
       throw new EwpBadRequestException(
@@ -123,39 +130,53 @@ public class EwpApiOutgoingMobilitiesV2Controller {
               + maxOmobilityIdsPerRequest);
     }
 
-    Map<OutgoingMobilitiesV2HostProvider, Collection<String>> providerToOmobilityIdsMap = getOmobilityIdsCoveredPerProviderOfHeiId(
-        sendingHeiId, outgoingMobilityIds);
-
     OmobilitiesGetResponseV2 response = new OmobilitiesGetResponseV2();
-    for (Map.Entry<OutgoingMobilitiesV2HostProvider, Collection<String>> entry : providerToOmobilityIdsMap.entrySet()) {
-      OutgoingMobilitiesV2HostProvider provider = entry.getKey();
-      Collection<String> coveredOmobilityIds = entry.getValue();
-      provider.findBySendingHeiIdAndOutgoingMobilityIds(
-              authenticationToken.getPrincipal().getHeiIdsCoveredByClient(), sendingHeiId,
-              coveredOmobilityIds)
-          .forEach(mobility -> response.getSingleMobilityObject().add(mobility));
+
+    // NOTE: The algorithm handles each Outgoing Mobility ID individually as it may be necessary to
+    // fall back to one or more providers.
+    for (String outgoingMobilityId : outgoingMobilityIds) {
+      List<OutgoingMobilitiesV2HostProvider> providersChain =
+          getProvidersChainForHeiAndOutgoingMobilityId(sendingHeiId, outgoingMobilityId);
+      for (OutgoingMobilitiesV2HostProvider possibleProvider : providersChain) {
+        Collection<StudentMobilityV2> providerResponse =
+            possibleProvider.findBySendingHeiIdAndOutgoingMobilityIds(
+                authenticationToken.getPrincipal().getHeiIdsCoveredByClient(),
+                sendingHeiId,
+                List.of(outgoingMobilityId));
+        if (!providerResponse.isEmpty()) {
+          StudentMobilityV2 mobilityObject = providerResponse.iterator().next();
+          response.getSingleMobilityObject().add(mobilityObject);
+          break;
+        }
+      }
     }
     return ResponseEntity.ok(response);
   }
 
-  private Map<OutgoingMobilitiesV2HostProvider, Collection<String>> getOmobilityIdsCoveredPerProviderOfHeiId(
-      String heiId, Collection<String> omobilityIds) {
-    Map<OutgoingMobilitiesV2HostProvider, Collection<String>> result = new HashMap<>();
-    for (String omobilityId : omobilityIds) {
-      Optional<EwpOutgoingMobilityMapping> mappingOptional = mappingRepository.findByHeiIdAndOmobilityId(
-          heiId, omobilityId);
-      if (mappingOptional.isPresent()) {
-        EwpOutgoingMobilityMapping mapping = mappingOptional.get();
+  private List<OutgoingMobilitiesV2HostProvider> getProvidersChainForHeiAndOutgoingMobilityId(
+      String heiId, String outgoingMobilityId) throws EwpUnknownHeiIdException {
 
-        Optional<OutgoingMobilitiesV2HostProvider> providerOptional = hostPluginManager.getSingleProvider(
-            heiId, mapping.getOunitId(), OutgoingMobilitiesV2HostProvider.class);
-        if (providerOptional.isPresent()) {
-          OutgoingMobilitiesV2HostProvider provider = providerOptional.get();
-          result.computeIfAbsent(provider, ignored -> new ArrayList<>());
-          result.get(provider).add(omobilityId);
-        }
-      }
+    if (!hostPluginManager.hasHostProvider(heiId, OutgoingMobilitiesV2HostProvider.class)) {
+      return new ArrayList<>();
     }
-    return result;
+
+    Optional<EwpOutgoingMobilityMapping> mappingOptional =
+        mappingRepository.findByHeiIdAndOmobilityId(heiId, outgoingMobilityId);
+    if (mappingOptional.isPresent()) {
+      EwpOutgoingMobilityMapping mapping = mappingOptional.get();
+      Optional<OutgoingMobilitiesV2HostProvider> providerOptional =
+          hostPluginManager.getSingleProvider(
+              heiId, mapping.getOunitId(), OutgoingMobilitiesV2HostProvider.class);
+      if (providerOptional.isPresent()) {
+        OutgoingMobilitiesV2HostProvider provider = providerOptional.get();
+        return List.of(provider);
+      } else {
+        throw new EwpUnknownOrganizationalUnitIdException(heiId, mapping.getOunitId());
+      }
+
+    } else {
+      return hostPluginManager.getPrimaryFollowedByNonPrimaryProviders(
+          heiId, OutgoingMobilitiesV2HostProvider.class);
+    }
   }
 }
