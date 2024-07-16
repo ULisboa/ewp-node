9259e2efdf070bce162dc1a291d19a1c5ce81392
Fábio Ferreira <fabioferreira@edu.ulisboa.pt>
2024-07-16 15:27:19 +0100

    refactor: remove support for V1 on EwpApiOutgoingMobilitiesManifestEntryProvider

--- backend/src/main/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesManifestEntryProvider.java
+++ backend/src/main/java/pt/ulisboa/ewp/node/api/ewp/controller/omobilities/EwpApiOutgoingMobilitiesManifestEntryProvider.java
@@ -2,12 +2,10 @@ package pt.ulisboa.ewp.node.api.ewp.controller.omobilities;
 
 import eu.erasmuswithoutpaper.api.architecture.v1.EmptyV1;
 import eu.erasmuswithoutpaper.api.architecture.v1.ManifestApiEntryBaseV1;
-import eu.erasmuswithoutpaper.api.omobilities.v1.OmobilitiesV1;
 import eu.erasmuswithoutpaper.api.omobilities.v2.OmobilitiesV2;
 import java.math.BigInteger;
 import java.util.Collection;
 import org.springframework.stereotype.Component;
-import pt.ulisboa.ewp.host.plugin.skeleton.provider.omobilities.OutgoingMobilitiesV1HostProvider;
 import pt.ulisboa.ewp.host.plugin.skeleton.provider.omobilities.OutgoingMobilitiesV2HostProvider;
 import pt.ulisboa.ewp.node.api.ewp.controller.EwpManifestEntryProvider;
 import pt.ulisboa.ewp.node.config.manifest.ManifestProperties;
@@ -22,33 +20,8 @@ public class EwpApiOutgoingMobilitiesManifestEntryProvider
     super(hostPluginManager, manifestProperties);
 
     super.registerHostProviderToManifestEntryConverter(
-        OutgoingMobilitiesV1HostProvider.class,
-        this::getManifestEntryForV1);
-
-    super.registerHostProviderToManifestEntryConverter(
         OutgoingMobilitiesV2HostProvider.class,
         this::getManifestEntryForV2);
-  }
-
-  public ManifestApiEntryBaseV1 getManifestEntryForV1(String heiId, String baseUrl,
-      Collection<OutgoingMobilitiesV1HostProvider> hostProviders) {
-    OmobilitiesV1 manifestEntry = new OmobilitiesV1();
-    manifestEntry.setVersion(hostProviders.iterator().next().getVersion());
-    manifestEntry.setAdminNotes(null);
-    manifestEntry
-        .setIndexUrl(baseUrl + EwpApiOutgoingMobilitiesV1Controller.BASE_PATH + "/index");
-    manifestEntry
-        .setGetUrl(baseUrl + EwpApiOutgoingMobilitiesV1Controller.BASE_PATH + "/get");
-
-    int maxOutgoingMobilityIdsPerRequest = hostProviders.stream().mapToInt(
-            OutgoingMobilitiesV1HostProvider::getMaxOutgoingMobilityIdsPerRequest)
-        .min().orElse(0);
-    manifestEntry.setMaxOmobilityIds(
-        BigInteger.valueOf(maxOutgoingMobilityIdsPerRequest));
-
-    manifestEntry.setHttpSecurity(getDefaultHttpSecurityOptions());
-    manifestEntry.setSendsNotifications(new EmptyV1());
-    return manifestEntry;
   }
 
   public ManifestApiEntryBaseV1 getManifestEntryForV2(String heiId, String baseUrl,
