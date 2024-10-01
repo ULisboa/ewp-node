ec4f56ab99e8773aa03a773a0b2444f0995c74e8
Fábio Ferreira <fabioferreira@edu.ulisboa.pt>
2024-10-01 11:59:00 +0100

    fix: XSRF protection on Admin API was not working due to Spring Boot v3 migration

--- backend/src/main/java/pt/ulisboa/ewp/node/api/admin/security/AdminApiSecurityConfig.java
+++ backend/src/main/java/pt/ulisboa/ewp/node/api/admin/security/AdminApiSecurityConfig.java
@@ -1,6 +1,12 @@
 package pt.ulisboa.ewp.node.api.admin.security;
 
 import com.google.common.collect.Lists;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,10 +21,17 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 import pt.ulisboa.ewp.node.api.admin.security.handler.AdminApiAuthenticationFailureHandler;
 import pt.ulisboa.ewp.node.api.admin.security.handler.AdminApiAuthenticationSuccessHandler;
 import pt.ulisboa.ewp.node.api.admin.security.handler.AdminApiLogoutSuccessHandler;
@@ -58,14 +71,13 @@ public class AdminApiSecurityConfig {
 
     // @formatter:off
     http.securityMatcher(AdminApiConstants.API_BASE_URI + "**")
-
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(AdminApiConstants.API_BASE_URI + "auth/login").permitAll()
-            .anyRequest().hasRole(ROLE_ADMIN)
-        )
-
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers(AdminApiConstants.API_BASE_URI + "auth/login")
+                    .permitAll()
+                    .anyRequest()
+                    .hasRole(ROLE_ADMIN))
         .userDetailsService(inMemoryUserDetailsManager())
-
         .formLogin(
             formLogin ->
                 formLogin
@@ -85,8 +97,12 @@ public class AdminApiSecurityConfig {
             exceptionHandling ->
                 exceptionHandling.authenticationEntryPoint(
                     new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        .csrf(
+            csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
     // @formatter:on
 
     return http.build();
@@ -109,5 +125,53 @@ public class AdminApiSecurityConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration(AdminApiConstants.API_BASE_URI + "**", configuration);
     return source;
+  }
+
+  static final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
+    private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+
+    @Override
+    public void handle(
+        HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
+      /*
+       * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
+       * the CsrfToken when it is rendered in the response body.
+       */
+      this.delegate.handle(request, response, csrfToken);
+    }
+
+    @Override
+    public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+      /*
+       * If the request contains a request header, use CsrfTokenRequestAttributeHandler
+       * to resolve the CsrfToken. This applies when a single-page application includes
+       * the header value automatically, which was obtained via a cookie containing the
+       * raw CsrfToken.
+       */
+      if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
+        return super.resolveCsrfTokenValue(request, csrfToken);
+      }
+      /*
+       * In all other cases (e.g. if the request contains a request parameter), use
+       * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
+       * when a server-side rendered form includes the _csrf request parameter as a
+       * hidden input.
+       */
+      return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+    }
+  }
+
+  static final class CsrfCookieFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+      CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+      // Render the token value to a cookie by causing the deferred token to be loaded
+      csrfToken.getToken();
+
+      filterChain.doFilter(request, response);
+    }
   }
 }
